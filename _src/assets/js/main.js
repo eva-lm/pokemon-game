de86c808e9cc04c6971bd1a343fadfd58e5aae67
eva-lm <eva.lm1810@gmail.com>
2019-08-19 22:35:53 +0200

    add api data and save array

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,73 @@
-'use strict';
+"use strict";
+const button = document.querySelector(".js-button");
+const input = document.querySelector("input");
+let pokemon = [];
 
-console.log('>> Ready :)');
+const inputFour = document.querySelector(".js-input-4");
+const inputSix = document.querySelector(".js-input-6");
+const inputEight = document.querySelector(".js-input-8");
+function getDataFromServer() {
+  console.log("Recogemos los datos de la Api y escuchamos el tamaño clickado");
+  pokemon = [];
+  if (inputFour.checked === true) {
+    inputSix.checked = false;
+    inputEight.checked = false;
+    return fetch(
+      `https://raw.githubusercontent.com/Adalab/cards-data/master/${
+        inputFour.value
+      }.json`
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        saveData(data);
+      });
+  } else if (inputSix.checked === true) {
+    inputFour.checked = false;
+    inputEight.checked = false;
+    return fetch(
+      `https://raw.githubusercontent.com/Adalab/cards-data/master/${
+        inputSix.value
+      }.json`
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        saveData(data);
+      });
+  } else if (inputEight.checked === true) {
+    inputSix.checked = false;
+    inputFour.checked = false;
+    return fetch(
+      `https://raw.githubusercontent.com/Adalab/cards-data/master/${
+        inputEight.value
+      }.json`
+    )
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        saveData(data);
+      });
+  }
+}
+
+function saveData(data) {
+  console.log("Los guardamos en una array");
+  for (const item of data) {
+    pokemon.push({
+      name: item.name,
+      image: item.image
+    });
+  }
+  console.log(pokemon);
+}
+function paintPokemon() {
+  console.log("Pintamos el listado segun tamaño de partida, 4, 6 o 8.");
+}
+
+function isFaceUp() {
+  console.log("");
+}
+function getClicked() {}
+
+button.addEventListener("click", getDataFromServer);
