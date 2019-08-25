cf337e6e329f300198cf40f416e6790cb3b26b40
eva-lm <eva.lm1810@gmail.com>
2019-08-25 12:05:50 +0200

    fix code and add functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,6 +2,7 @@
 const button = document.querySelector(".js-button");
 const input = document.querySelectorAll("input");
 let pokemon = [];
+let pokeUp = [];
 
 function getDataFromServer() {
   console.log("Recogemos los datos de la Api y escuchamos el tamaño clickado");
@@ -9,13 +10,11 @@ function getDataFromServer() {
   for (let i = 0; i < input.length; i++) {
     if (input[i].checked) {
       return fetch(
-        `https://raw.githubusercontent.com/Adalab/cards-data/master/${
-          input[i].value
-        }.json`
+        `https://raw.githubusercontent.com/Adalab/cards-data/master/${input[i].value}.json`
       )
         .then(response => response.json())
         .then(data => {
-          console.log(data);
+          //console.log(data);
           saveData(data);
           paintPokemon();
           listenPokemon();
@@ -26,9 +25,12 @@ function getDataFromServer() {
 function saveData(data) {
   console.log("Los guardamos en una array");
   for (const item of data) {
-    pokemon.push({
+    pokeUp.push({
       name: item.name,
       image: item.image
+    });
+    pokemon.push({
+      image: "https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB"
     });
   }
 }
@@ -37,35 +39,72 @@ function paintPokemon() {
   const pokemonList = document.querySelector(".js-list");
   let text = "";
   for (let pokemonIndex = 0; pokemonIndex < pokemon.length; pokemonIndex++) {
-    text += `<li class="js-li">
+    text += `<li class="js-li" data-index="${pokemonIndex}">
         <img src="${pokemon[pokemonIndex].image}">
-        <p>${pokemon[pokemonIndex].name}</p>
         </li>`;
   }
   pokemonList.innerHTML = text;
   console.log(pokemonList);
-  console.log("Pintamos el listado segun tamaño de partida, 4, 6 o 8.");
+  console.log(
+    "Recorrremos array pokemon y pintamos el listado segun tamaño de partida, 4, 6 o 8."
+  );
 }
 
 function listenPokemon() {
   const pokemonItems = document.querySelectorAll(".js-li");
   console.log(pokemonItems);
-  for (let pokemonIndex = 0; pokemonIndex < 0; pokemonIndex++) {
+  for (
+    let pokemonIndex = 0;
+    pokemonIndex < pokemonItems.length;
+    pokemonIndex++
+  ) {
     pokemonItems[pokemonIndex].addEventListener("click", getClicked);
   }
   console.log(
-    "recogemos todo el listado que se pinta con los resultados para escucharlos"
+    "recorremos el listado que se pinta con los resultados para escucharlos"
   );
 }
 
 function getClicked(ev) {
-  const pokemonCurrentTarjet = ev.currentTarjet.pokemonCurrentTarjet;
+  const index = parseInt(ev.currentTarget.dataset.index);
+  const pokemonItems = document.querySelectorAll(".js-li");
+  console.log("pokemonItems", pokemonItems);
+  pokemonItems[index].classList.add("select");
 
-  console.log();
+  console.log(index);
+  pokemonUp(index);
+  listenPokemon();
 }
 
-function isFaceUp() {
-  console.log("");
+function pokemonUp(index) {
+  console.log("poner pokemon dados la vuelta");
+  const pokemonList = document.querySelector(".js-list");
+  let text = "";
+  const pokemonItems = document.querySelectorAll(".js-li");
+
+  //for (let i = 0; i < pokeUp.length; i++) {
+  if (pokemonItems[index]) {
+    for (let i = 0; i < pokeUp.length; i++) {
+      text += `<li class="js-li-up">
+        <img src="${pokeUp[i].image}">
+        <p>${pokeUp[i].name}</p></li>`;
+    }
+  }
+  pokemonList.innerHTML = text;
+
+  console.log(pokeUp);
 }
+
+/* function paintFaceUp() {
+  const pokemonList = document.querySelector(".js-list");
+  let text = "";
+  for (let i = 0; i < pokeUp.length; i++) {
+    text += `<li class="js-li-up" data-index="${i}">
+      <img src="${pokeUp[i].image}">
+      <p>${pokeUp[i].name}</p>;
+      </li>`;
+  }
+  pokemonList.innerHTML = text;
+} */
 
 button.addEventListener("click", getDataFromServer);
